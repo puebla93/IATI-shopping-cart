e34ecaf250b828dcee48344d6842411b139f720a
Javier Puebla <jpuebla1993@gmail.com>
2023-02-27 18:46:56 +0100

    add urls to add product and view cart

--- products/urls.py
+++ products/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
         r"^products/(?P<pk>[0-9]+)/$", views.ProductRetrieveUpdateDestroy.as_view(),
         name="product_retrieve_update_destroy"
     ),
+    re_path(
+        r"^add_product/$", views.CartItemCreate.as_view(),
+        name="add_product_view"
+    ),
+    re_path(
+        r"^view_cart/$", views.ShoppingCartView.as_view(),
+        name="add_product_view"
+    ),
 ]
