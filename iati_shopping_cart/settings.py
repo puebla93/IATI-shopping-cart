582a12cadf0881db10669e26cbcae0fcb7e6e1fc
Javier Puebla <jpuebla1993@gmail.com>
2023-02-28 23:22:18 +0100

    send emails throught console

--- iati_shopping_cart/settings.py
+++ iati_shopping_cart/settings.py
@@ -114,6 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
