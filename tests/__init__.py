888bef157d8d037ed24a84112d5a7054fc75807f
Javier Puebla <jpuebla1993@gmail.com>
2023-02-28 11:19:05 +0100

    add products pytest fixtures

---
+++ tests/__init__.py
@@ -0,0 +1,60 @@
+import pytest
+import random
+from datetime import datetime
+
+from products.models import Product, Cap, Tshirt, ShoppingCart
+
+
+@pytest.fixture
+def product():
+    return Product.objects.create(
+        product_type=random.choice(Product.PRODUCT_TYPES),
+        main_color='red',
+        secondary_colors='blue, green',
+        brand='Acme',
+        inclusion_date=datetime.utcnow().date(),
+        photo_url='https://example.com/product.png',
+        unit_price=10.0,
+        initial_stock=100,
+        current_stock=80
+    )
+
+
+@pytest.fixture
+def cap_product():
+    return Cap.objects.create(
+        product_type=Product.CAP,
+        main_color='red',
+        secondary_colors='blue, green',
+        brand='Acme',
+        inclusion_date=datetime.utcnow().date(),
+        photo_url='https://example.com/cap.png',
+        unit_price=10.0,
+        initial_stock=100,
+        current_stock=80,
+        logo_color="black"
+    )
+
+
+@pytest.fixture
+def tshirt_product():
+    return Tshirt.objects.create(
+        product_type=Product.TSHIRT,
+        main_color='red',
+        secondary_colors='blue, green',
+        brand='Acme',
+        inclusion_date=datetime.utcnow().date(),
+        photo_url='https://example.com/tshirt.png',
+        unit_price=10.0,
+        initial_stock=100,
+        current_stock=80,
+        size="L",
+        composition={"cotton": 50, "polyester": 50},
+        gender="Man",
+        has_sleeves=True
+    )
+
+
+@pytest.fixture
+def shopping_cart():
+    return ShoppingCart.objects.create()
