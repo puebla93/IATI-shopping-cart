70694413d64f733a1e22c631b505dff1209bc71e
Javier Puebla <jpuebla1993@gmail.com>
2023-03-01 12:43:07 +0100

    load data on app startup

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "products"
+
+    def ready(self):
+        # Clean the database
+        # call_command("flush")
+        # Load initial data
+        call_command("loaddata", "initial_stock.yaml")
