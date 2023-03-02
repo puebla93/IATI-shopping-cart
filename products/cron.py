ea2a88f6ec13d279d71f8126730d1af15fa9361d
Javier Puebla <jpuebla1993@gmail.com>
2023-03-02 17:24:42 +0100

    improve to cron job

--- products/cron.py
+++ products/cron.py
@@ -14,7 +14,10 @@ def update_product_stock() -> None:
         Q(shopping_cart__purchased=True) | Q(shopping_cart__created_on=datetime.utcnow().today())
     ).values('product_id').annotate(total=Sum('quantity'))
 
-    for product_quantity in products_quantity:
-        product = Product.objects.get(id=product_quantity["product_id"])
-        product.current_stock = product.initial_stock - product_quantity["total"]
+    products = Product.objects.filter(
+        id__in=[product_quantity["product_id"] for product_quantity in products_quantity]
+    )
+
+    for i, product in enumerate(products):
+        product.current_stock = product.initial_stock - products_quantity[i]["total"]
         product.save(update_fields=["current_stock"])
