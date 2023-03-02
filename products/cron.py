347ae210ea4a853692e487b1d56ffd3b15244fa0
Javier Puebla <jpuebla1993@gmail.com>
2023-03-02 16:04:49 +0100

    add cron function to update product stock

--- products/cron.py
+++ products/cron.py
@@ -1,7 +1,16 @@
-from products.models import Product, ShoppingCart
+from datetime import datetime
+
+from django.db.models import Q, Sum
+
+from products.models import Product, CartItem
 
 
 def update_product_stock():
-    # Calculate new stock and update the database
-    # ...
-    pass
+    products_quantity = CartItem.objects.filter(
+        Q(shopping_cart__purchased=True) | Q(shopping_cart__created_on=datetime.utcnow().today())
+    ).values('product_id').annotate(total=Sum('quantity'))
+
+    for product_quantity in products_quantity:
+        product = Product.objects.get(id=product_quantity["product_id"])
+        product.current_stock = product.initial_stock - product_quantity["total"]
+        product.save(update_fields=["current_stock"])
